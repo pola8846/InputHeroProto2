7521c02be7ae36d6a2e8d4ab447826e4b6ca1fa6
pola8846 <75826137+pola8846@users.noreply.github.com>
2024-04-24 15:06:28 +0900

    적 FSM 기본 완성

--- Assets/Scripts/Test/TestEnemy_Air_R_1.cs
+++ Assets/Scripts/Test/TestEnemy_Air_R_1.cs
@@ -12,13 +12,34 @@ public class TestEnemy_Air_R_1 : Enemy
     [SerializeField]
     private float patrolTick = 1f;
     [SerializeField]
-    private float patrolMinRange = 2f;//한 번 움직일때 최소
-    [SerializeField]
-    private float patrolMaxRange = 4f;//한 번 움직일 때 최대
-    [SerializeField]
-    private float patrolRangeTotal = 5f;//첫 위치
+    private float patrolRange = 5f;//첫위치에서 얼마나 순찰할건지
     private Vector2 patrolDist;
     private Vector2 patrolOrigin;
+    [SerializeField]
+    private float attackRange2 = 5f;
+    [SerializeField]
+    private float attackRange3 = 1f;
+    [SerializeField, Range(0f, 90f)]
+    private float attackRangeAngle = 45f;
+    [SerializeField, Range(1, 10)]
+    private int attackMoveCount = 3;
+    private int attackMoveCounter = 0;
+    [SerializeField, Range(1, 20)]
+    private int attackCount = 2;
+    private int attackCounter = 0;
+    [SerializeField]
+    private float attackMoveSpeedRate = 4f;
+    [SerializeField]
+    private float attackMoveWaitTime = 0.2f;
+    [SerializeField]
+    private float attackDelay = .35f;
+    [SerializeField]
+    private float attackWaitTimeBeforeAttack = 1f;
+    [SerializeField]
+    private float attackWaitTimeAfterAttack = 2f;
+    private Vector2 attackPosOrigin;
+    private Vector2 attackPos;
+    private BulletShooter shooter;
     [SerializeField]
     private int state = 0;
     private Renderer renderer;
@@ -27,15 +48,18 @@ public class TestEnemy_Air_R_1 : Enemy
     private float timer1;
     private float timer2;
     private float timer3;
+    private float timer4;
     protected override void Start()
     {
         base.Start();
         rb = GetComponent<Rigidbody2D>();
         renderer = GetComponent<Renderer>();
+        shooter = GetComponent<BulletShooter>();
         originColor = renderer.material.color;
         timer1 = Time.time;
         timer2 = Time.time;
         timer3 = Time.time;
+        timer4 = Time.time;
         patrolDist = transform.position;
         patrolOrigin = transform.position;
     }
@@ -56,19 +80,70 @@ public class TestEnemy_Air_R_1 : Enemy
                 }
                 break;
             case 1://순찰 이동
-                if (Vector3.Distance(transform.position, (Vector3)patrolDist) <= .3f)
+                if (Vector2.Distance(transform.position, (Vector3)patrolDist) <= .1f)
                 {
                     SetState(0);
                 }
                 break;
             case 2://순찰 복귀
-                if (Vector3.Distance(transform.position, (Vector3)patrolDist) <= .3f)
+                if (Vector2.Distance(transform.position, (Vector3)patrolDist) <= .1f)
                 {
                     SetState(0);
                 }
                 break;
             case 3://플레이어에게 접근
-
+                if (Vector2.Distance(transform.position, (Vector3)attackPos) <= .1f)
+                {
+                    SetState(4);
+                }
+                break;
+            case 4://공격 영역 내에서 재이동
+                if (Vector2.Distance(transform.position, (Vector3)attackPos) <= .1f)
+                {
+                    if (attackMoveCounter >= attackMoveCount)
+                    {
+                        SetState(6);
+                    }
+                    else
+                    {
+                        SetState(5);
+                    }
+                }
+                break;
+            case 5://이동 도중 대기
+                if (timer2 + attackMoveWaitTime <= Time.time)
+                {
+                    SetState(4);
+                }
+                break;
+            case 6://공격 이전 대기
+                if (timer3 + attackWaitTimeBeforeAttack <= Time.time)
+                {
+                    SetState(7);
+                }
+                break;
+            case 7:
+                if (attackCounter >= attackCount)
+                {
+                    if (timer4 + attackWaitTimeAfterAttack <= Time.time)
+                    {
+                        if (FindPlayer())
+                        {
+                            SetState(3);
+                        }
+                        else
+                        {
+                            SetState(2);
+                        }
+                    }
+                }
+                else
+                {
+                    if (timer4 + attackDelay <= Time.time)
+                    {
+                        SetState(7);
+                    }
+                }
                 break;
             default:
                 break;
@@ -80,27 +155,33 @@ public class TestEnemy_Air_R_1 : Enemy
 
 
         var player = GameManager.Player;
-        bool isRight = player.transform.position.x >= transform.position.x;//플레이어가 오른쪽에 있는가?
+        //bool isRight = player.transform.position.x >= transform.position.x;//플레이어가 오른쪽에 있는가?
 
         switch (state)
         {
-            case 0:
-                rb.velocity = new Vector2(0, 0);
-                break;
             case 1:
                 {
                     float speed = Mathf.Max(0, Speed) * patrolSpeedRate;
-                    rb.velocity = GetDist(patrolDist) * speed;
+                    MoveTo(patrolDist, speed);
                 }
                 break;
             case 2:
                 {
                     float speed = Mathf.Max(0, Speed);
-                    rb.velocity = GetDist(patrolDist) * speed;
+                    MoveTo(patrolDist, speed);
                 }
                 break;
             case 3:
-
+                {
+                    float speed = Mathf.Max(0, Speed);
+                    MoveTo(attackPos, speed);
+                }
+                break;
+            case 4:
+                {
+                    float speed = Mathf.Max(0, Speed) * attackMoveSpeedRate;
+                    MoveTo(attackPos, speed);
+                }
                 break;
             default:
                 break;
@@ -117,7 +198,15 @@ public class TestEnemy_Air_R_1 : Enemy
                 break;
             case 2:
                 break;
-            case 3:
+            case 4:
+                attackMoveCounter++;
+                rb.velocity = new Vector2(0, 0);
+                break;
+            case 6:
+                attackCounter = 0;
+                break;
+            case 7:
+                attackCounter++;
                 break;
             default:
                 break;
@@ -128,6 +217,7 @@ public class TestEnemy_Air_R_1 : Enemy
         switch (state)
         {
             case 0:
+                rb.velocity = Vector3.zero;
                 timer1 = Time.time;
                 break;
             case 1:
@@ -137,6 +227,27 @@ public class TestEnemy_Air_R_1 : Enemy
                 patrolDist = patrolOrigin;
                 break;
             case 3:
+                SetAttackPosOrigin();
+                SetAttackPos();
+                attackMoveCounter = 0;
+                break;
+            case 4:
+                SetAttackPos();
+                break;
+            case 5:
+                rb.velocity = Vector3.zero;
+                timer2 = Time.time;
+                break;
+            case 6:
+                timer3 = Time.time;
+                break;
+            case 7:
+                timer4 = Time.time;
+                //공격
+                Vector2 temp = GetDist(GameManager.Player.transform.position);
+                shooter.bulletAngleMax = shooter.bulletAngleMin =
+                    Vector2.SignedAngle(Vector2.up, temp);
+                shooter.triger = true;
                 break;
             default:
                 break;
@@ -162,8 +273,34 @@ public class TestEnemy_Air_R_1 : Enemy
 
     private void SetRandomPatrolPosition()
     {
-        float randomVelue = Random.Range(1,1);
-        float targetX = transform.position.x + Random.Range(-patrolDistance, patrolDistance);
+        float randomVelue = Random.Range(-patrolRange, patrolRange);
+        float targetX = patrolOrigin.x + randomVelue;
         patrolDist = new Vector2(targetX, transform.position.y);
+    }
+
+    private void SetAttackPosOrigin()
+    {
+        Vector3 playerPos = GameManager.Player.transform.position;
+        Vector3 direction = Quaternion.AngleAxis(Random.Range(-attackRangeAngle, attackRangeAngle), Vector3.forward) * Vector3.up;
+        attackPosOrigin = playerPos + (direction * attackRange2);
+    }
+    private void SetAttackPos()
+    {
+        float x = Random.Range(-attackRange3, attackRange3);
+        float y = Random.Range(-attackRange3, attackRange3);
+        attackPos = new Vector2(attackPosOrigin.x + x, attackPosOrigin.y + y);
+    }
+
+    private void MoveTo(Vector2 targetPos, float speed)
+    {
+        if (Vector2.Distance(targetPos, (Vector2)transform.position) <= speed * Time.fixedDeltaTime)
+        {
+            transform.position = targetPos;
+            rb.velocity = Vector3.zero;
+        }
+        else
+        {
+            rb.velocity = GetDist(targetPos) * speed;
+        }
     }
 }
