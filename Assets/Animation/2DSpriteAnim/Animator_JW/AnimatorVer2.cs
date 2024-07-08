143f441284de6c8b900df26d55e70bc1356a52b0
Squidette <qfkrwldnjs@naver.com>
2024-07-08 18:35:19 +0900

    애니메이터 버전 2 기본틀

--- Assets/Animation/2DSpriteAnim/Animator_JW/AnimatorVer2.cs
+++ Assets/Animation/2DSpriteAnim/Animator_JW/AnimatorVer2.cs
@@ -1,62 +1,35 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+
+/// <summary>
+/// 지원: 첫 번째와 마지막 스프라이트 인덱스만 입력하면 알아서 이미지를 뽑아주는 애니메이터
+/// </summary>
 
 public class AnimatorVer2 : MonoBehaviour
 {
-    public enum AnimType
-    {
-        AIM,
-        RELOAD,
-        DASH,
-        DIE
-    }
-
     public string spriteFileName;
 
-    SpriteRenderer spriteRenderer;
-    Sprite[] sprites;
+    protected SpriteRenderer spriteRenderer;
+    protected Sprite[] sprites;
 
-    public AnimType currentAnimation;
-    private AnimType cache;
+    protected AnimationVer2 currentAnimationInst;
 
-    public Dictionary<AnimType, AnimationVer2> animations;
-
-    AnimationVer2 currentAnimationInst;
-
-    void Start()
+    protected virtual void Start()
     {
         spriteRenderer = GetComponent<SpriteRenderer>();
         sprites = Resources.LoadAll<Sprite>(spriteFileName);
-
-        currentAnimationInst = animations[currentAnimation];
     }
 
-    void Update()
+    protected virtual void Update()
     {
-        // 매 프레임 캐시데이터와 비교해서 현재 애니메이션을 선택
-        if (cache != currentAnimation)
-        {
-            currentAnimationInst?.Exit();
-
-            //currentAnimationInst = FindAnimation(animations, currentAnimation);
-            cache = currentAnimation;
-
-            currentAnimationInst?.Enter();
-        }
-
-        currentAnimationInst?.Run();
-
         SpriteRendererUpdate();
     }
 
-    // 애니메이션의 정보를 스프라이트 렌더러에 반영합니다
     void SpriteRendererUpdate()
     {
-        if (spriteRenderer == null) return;
+        if (currentAnimationInst == null) return;
 
-        // 스프라이트 반영
+        // currentAnimationInst의 정보를 스프라이트에 반영
         spriteRenderer.sprite = sprites[currentAnimationInst.GetSpriteListIndex()];
+        spriteRenderer.flipX = currentAnimationInst.flip;
     }
 }
---
+++ Assets/Animation/2DSpriteAnim/Animator_JW/PlayerUpperAnimator.cs
@@ -0,0 +1,76 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerUpperAnimator : AnimatorVer2
+{
+    //-------------------------------(상속해서 애니메이터 생성시 바꿔야할 정보)-------------------------------//
+    public enum AnimType
+    {
+        AIM,
+        RELOAD,
+        DIE,
+        DASH,
+        // ...
+    }
+
+    [ContextMenu("일반 스프라이트 애니메이션 추가")]
+    void AddBasicSpriteAnim() { animations.Add(new SerializablePair(AnimType.RELOAD, new BasicSpriteAnimVer2())); }
+    // ...
+    //------------------------------------(아래부터는 그냥 복붙해도됨,,)------------------------------------//
+
+    [Serializable]
+    public class SerializablePair
+    {
+        public SerializablePair(AnimType t, AnimationVer2 a)
+        {
+            type = t;
+            anim = a;
+        }
+
+        public AnimType type;
+
+        [SerializeReference]
+        public AnimationVer2 anim;
+    }
+
+    private AnimType cacheIndex;
+    public AnimType currentAnimation;
+
+    public List<SerializablePair> animations = new List<SerializablePair>();
+
+    protected override void Start()
+    {
+        base.Start();
+
+        currentAnimationInst = GetAnimationInst(currentAnimation);
+    }
+
+    protected override void Update()
+    {
+        // 매 프레임 캐시데이터와 비교해서 현재 애니메이션을 선택
+        if (cacheIndex != currentAnimation)
+        {
+            currentAnimationInst?.Exit();
+
+            currentAnimationInst = GetAnimationInst(currentAnimation);
+            cacheIndex = currentAnimation;
+
+            currentAnimationInst?.Enter();
+        }
+
+        currentAnimationInst?.Run();
+
+        base.Update();
+    }
+
+    AnimationVer2 GetAnimationInst(AnimType type)
+    {
+        // 타입이 일치하는 애니메이션중 인덱스가 낮은것을 리턴(= 타입 중복이 있으면 뒤에있는것은 무시됨)
+        foreach (SerializablePair pair in animations)
+        {
+            if (pair.type == type) return pair.anim;
+        }
+        return null;
+    }
+}
