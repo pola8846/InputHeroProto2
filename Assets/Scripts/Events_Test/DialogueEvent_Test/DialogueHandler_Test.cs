b11d0d0ae516318d93b812148ad3e7f0cbac7a48
Squidette <qfkrwldnjs@naver.com>
2024-07-22 14:45:01 +0900

    글상자 서식 스크립터블오브젝트는 삭제 (그냥 프리팹으로 대체)

--- Assets/Scripts/Events_Test/DialogueEvent_Test/DialogueHandler_Test.cs
+++ Assets/Scripts/Events_Test/DialogueEvent_Test/DialogueHandler_Test.cs
@@ -14,20 +14,20 @@ public class DialogueHandler_Test : CustomHandler_Test
     public DialoguesSO_Test dialogueSO;
 
     // 글상자 타입
-    public DialogueBoxType boxTypeSO;
+    //public DialogueBoxType boxTypeSO;
 
     Canvas canvas;
-    GameObject UIPrefab;
+    public GameObject UIPrefab;
 
-    // 글상자 인스턴스
-    GameObject inst;
+    // UIPrefab의 인스턴스
+    GameObject UIInst;
 
     // 대화 끝내기 가능 화살표 이미지
     GameObject nextArrow;
     Tween arrowBounceAnim;
 
     // 글자 하나씩 나타나게 하는 거
-    TypewriterCore typeWriter; // 이게 한국어로도 이쁘게 보일지는 잘 모르겠다
+    TypewriterCore typeWriter;
     bool skipped = false;
 
     public override void Enter()
@@ -36,28 +36,25 @@ public class DialogueHandler_Test : CustomHandler_Test
         skipped = false;
 
         canvas = GameObject.Find("Canvas").GetComponent<Canvas>();
-        UIPrefab = Resources.Load<GameObject>("DialogueBox");
 
         if (UIPrefab == null || canvas == null)
         {
-            Debug.Log("캔버스와 UI 프리팹을 찾지 못함 -> DialogueHandler_Test 강제종료!");
+            Debug.Log("캔버스나 UI 프리팹을 찾지 못함 -> DialogueHandler_Test 강제종료!");
             lifeCycleBools.isDone_This = true;
             return;
         }
 
         // 컴포넌트 가져오기
-        inst = Object.Instantiate(UIPrefab, canvas.transform);
-        TextMeshProUGUI tmp = inst.transform.Find("Text (TMP)").gameObject.GetComponent<TextMeshProUGUI>();
-        typeWriter = inst.transform.Find("Text (TMP)").gameObject.GetComponent<TypewriterCore>();
-        nextArrow = inst.transform.Find("Next").gameObject;
+        UIInst = Object.Instantiate(UIPrefab, canvas.transform);
+        TextMeshProUGUI tmp = UIInst.transform.Find("Text (TMP)").gameObject.GetComponent<TextMeshProUGUI>();
+        typeWriter = UIInst.transform.Find("Text (TMP)").gameObject.GetComponent<TypewriterCore>();
+        nextArrow = UIInst.transform.Find("Next").gameObject;
 
         // 컴포넌트에 설정 적용하기
         DialogueInfo info = dialogueSO.dialogues[dialogueIndex];
 
-        inst.GetComponent<Image>().sprite = boxTypeSO.BoxImage;
-        inst.GetComponent<RectTransform>().anchoredPosition = info.rectPosition;
+        UIInst.GetComponent<RectTransform>().anchoredPosition = info.rectPosition;
 
-        tmp.fontSize = boxTypeSO.FontSize;
         tmp.gameObject.GetComponent<RectTransform>().anchoredPosition = info.textLocalPosition;
         
         typeWriter.ShowText(info.text); //tmp.text = info.text;
@@ -87,14 +84,17 @@ public class DialogueHandler_Test : CustomHandler_Test
         if (skipped) return;
 
         // 화살표 이미지 켜기
-        nextArrow.GetComponent<Image>().enabled = true;
+        if (nextArrow != null)
+        {
+            nextArrow.GetComponent<Image>().enabled = true;
 
-        // 화살표 바운스 시키기
-        float bounceDistance = 17.0F;
-        float bounceSpeed = 0.3F;
-        Vector2 bounceTarget = nextArrow.GetComponent<RectTransform>().anchoredPosition;
-        bounceTarget.x += bounceDistance;
-        arrowBounceAnim = nextArrow.GetComponent<RectTransform>().DOAnchorPos(bounceTarget, bounceSpeed).SetLoops(-1, LoopType.Yoyo);
+            // 화살표 바운스 시키기
+            float bounceDistance = 17.0F;
+            float bounceSpeed = 0.3F;
+            Vector2 bounceTarget = nextArrow.GetComponent<RectTransform>().anchoredPosition;
+            bounceTarget.x += bounceDistance;
+            arrowBounceAnim = nextArrow.GetComponent<RectTransform>().DOAnchorPos(bounceTarget, bounceSpeed).SetLoops(-1, LoopType.Yoyo);
+        }
 
         skipped = true;
     }
@@ -102,7 +102,7 @@ public class DialogueHandler_Test : CustomHandler_Test
     protected override void Exit() // 나갈때 한번 실행할 코드
     {
         if (arrowBounceAnim != null) arrowBounceAnim.Kill();
-        Object.Destroy(inst);
+        Object.Destroy(UIInst);
 
         base.Exit();
         //skipped = false;
