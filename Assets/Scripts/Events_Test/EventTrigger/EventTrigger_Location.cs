dd8bd21b9ce4330e0d2003698ff8ba8d6a07dd63
Squidette <qfkrwldnjs@naver.com>
2024-07-22 15:12:31 +0900

    트리거 장소 생성

---
+++ Assets/Scripts/Events_Test/EventTrigger/EventTrigger_Location.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventTrigger_Location : MonoBehaviour
+{
+    // 타겟이 콜라이더에 들어오면 이벤트 호출
+    public GameObject target;
+    public int eventIDToCall;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (target == null) return;
+
+        if (collision.gameObject == target)
+        {
+            Debug.Log(eventIDToCall + "번 이벤트 호출");
+        }
+    }
+}
